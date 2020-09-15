3c31a2b6333e492577dd0f5d6964a6b8b6c7fac2
csdoker <csdoker@sfmail.sf-express.com>
2020-09-15 18:05:22 +0800

    refactor(Button): modify style

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <v-button :loading="true">按钮</v-button>
+    <div class="container">
+      <h2>Button 按钮</h2>
+      <v-button :loading="true">按钮</v-button>
+    </div>
   </div>
 </template>
 
@@ -11,12 +14,28 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+.container {
+  max-width: 800px;
+  margin: 30px auto;
+  border-radius: 4px;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.3);
+  padding: 16px;
+}
+.container h2 {
+  margin: 20px 0;
+}
+
 </style>
--- packages/button/src/index.vue
+++ packages/button/src/index.vue
@@ -1,11 +1,10 @@
 <template>
   <button
     class="v-button"
-    :class="{ [`icon-${iconPosition}`]: true }"
     @click="$emit('click')"
   >
-    <v-icon class="icon" v-if="icon && !loading" :name="icon"></v-icon>
-    <v-icon class="loading icon" v-if="loading" name="loading"></v-icon>
+    <v-icon class="v-icon" v-if="icon && !loading" :name="icon"></v-icon>
+    <v-icon class="loading v-icon" v-if="loading" name="loading"></v-icon>
     <div class="v-button-content">
       <slot></slot>
     </div>
@@ -17,73 +16,21 @@ import VIcon from '~/icon/src/index.vue'
 
 export default {
   name: 'VButton',
-  // props: ['icon', 'iconPosition']
   components: {
     'v-icon': VIcon
   },
   props: {
-    icon: {},
+    icon: {
+      type: String
+    },
     loading: {
       type: Boolean,
       default: false
-    },
-    iconPosition: {
-      type: String,
-      default: 'left',
-      validator (value) {
-        return value === 'left' || value === 'right'
-      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import "styles/_var";
-
-.v-button {
-  font-size: $font-size;
-  height: $button-height;
-  padding: 0 1em;
-  border-radius: $border-radius;
-  border: 1px solid $border-color;
-  background: $button-bg;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  vertical-align: middle;
-  &:hover {
-    border-color: $border-color-hover;
-  }
-  &:active {
-    background-color: $button-active-bg;
-  }
-  &:focus {
-    outline: none;
-  }
-  > .v-button-content {
-    order: 2;
-    // line-height: var(--line-height);
-  }
-  > .icon {
-    order: 1;
-    margin-right: 0.1em;
-  }
-  &.icon-right {
-    > .v-button-content {
-      order: 1;
-    }
-    > .icon {
-      order: 2;
-      margin-right: 0;
-      margin-left: 0.1em;
-    }
-  }
-  .loading {
-    @include spin;
-  }
-  & + & {
-    margin-left: 4px;
-  }
-}
+@import './index.scss'
 </style>
