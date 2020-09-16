1f3a3d4397d10cae19c0b0f7eeef8ebed01de632
csdoker <csdoker@sfmail.sf-express.com>
2020-09-16 09:44:51 +0800

    feat: add type button style

--- examples/App.vue
+++ examples/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div class="container">
       <h2>Button 按钮</h2>
+      <v-button type="primary">Primary</v-button>
       <v-button>Default</v-button>
+      <v-button type="dashed">Dashed</v-button>
+      <v-button type="danger">Dashed</v-button>
       <v-button :loading="true">Default</v-button>
     </div>
   </div>
